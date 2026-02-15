5ceaff6677782380ba4d80a0a4afac6cc4cfac06
fd-Martin <fdmosharof@gmail.com>
2026-02-15 11:25:14 +0600

    theme toggle created

--- src/Page/Shared/Navbar/Navbar.jsx
+++ src/Page/Shared/Navbar/Navbar.jsx
@@ -1,86 +1,19 @@
-// import React from "react";
-// import Logo from "../../../Components/Logo/Logo";
-// import { NavLink } from "react-router";
-
-// const Navbar = () => {
-//   const links = (
-//     <>
-//       <li>
-//         <NavLink to="/">Home</NavLink>
-//       </li>
-//       <li>
-//         <NavLink to="/allbooks">All Books</NavLink>
-//       </li>
-
-//       <li>
-//         <a>Item 3</a>
-//       </li>
-//     </>
-//   );
-//   return (
-//     <div className="max-w-7xl mx-auto w-full flex-1">
-//       <div className="navbar bg-base-100 shadow-sm">
-//         <div className="navbar-start">
-//           <div className="dropdown">
-//             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-//               <svg
-//                 xmlns="http://www.w3.org/2000/svg"
-//                 className="h-5 w-5"
-//                 fill="none"
-//                 viewBox="0 0 24 24"
-//                 stroke="currentColor"
-//               >
-//                 {" "}
-//                 <path
-//                   strokeLinecap="round"
-//                   strokeLinejoin="round"
-//                   strokeWidth="2"
-//                   d="M4 6h16M4 12h8m-8 6h16"
-//                 />{" "}
-//               </svg>
-//             </div>
-//             <ul
-//               tabIndex="-1"
-//               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-//             >
-//               {links}
-//             </ul>
-//           </div>
-//           <Logo></Logo>
-//         </div>
-//         <div className="navbar-center hidden lg:flex">
-//           <ul className="menu menu-horizontal px-1">{links}</ul>
-//         </div>
-//         <div className="navbar-end">
-//           <a className="btn">Button</a>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Navbar;
-
-
-import React  from "react";
-
+import React from "react";
 import { Link, NavLink, useNavigate } from "react-router";
-
-// import ThemeToggle from "../ThemeToggle/ThemeToggle";
 import { Tooltip } from "react-tooltip";
 import "react-tooltip/dist/react-tooltip.css";
 import useAuth from "../../../hooks/useAuth";
+import ThemeToggle from "../../../Components/ThemeToggle/ThemeToggle";
 
 const Navbar = () => {
   const { user, signOutUser, loading } = useAuth();
   const navigate = useNavigate();
 
   const handleSignOut = () => {
-    signOutUser()
-      .then(() => {
-        // toast.success("Logged out successfully!");
-        navigate("/");
-      })
+    signOutUser().then(() => {
+      // toast.success("Logged out successfully!");
+      navigate("/");
+    });
     //   .catch((err) => toast.error(err.message));
   };
 
@@ -108,30 +41,6 @@ const Navbar = () => {
           }
         >
           All Books
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/add-a-book"
-          className={({ isActive }) =>
-            `px-2 py-1 ${
-              isActive ? "text-blue-600 font-bold" : "hover:text-blue-600"
-            }`
-          }
-        >
-          Add A Book
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/my-books"
-          className={({ isActive }) =>
-            `px-2 py-1 ${
-              isActive ? "text-blue-600 font-bold" : "hover:text-blue-600"
-            }`
-          }
-        >
-          My Books
         </NavLink>
       </li>
     </>
@@ -168,21 +77,6 @@ const Navbar = () => {
 
             {user ? (
               <>
-                {/* Profile Avatar */}
-                {/* <div className="relative group flex items-center">
-                  <img
-                    src={
-                      user?.photoURL ||
-                      "https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png"
-                    }
-                    alt="avatar"
-                    className="w-10 h-10 rounded-full border cursor-pointer md:hidden"
-                  />
-                  <p className="text-xs font-bold px-2 py-1">
-                    {user?.displayName || "User"}
-                  </p>
-                </div> */}
-
                 <div className="relative group hidden md:block">
                   <img
                     src={
@@ -208,7 +102,7 @@ const Navbar = () => {
             ) : (
               <>
                 <NavLink
-                  to="/login"
+                  to="authentication/login"
                   className="btn btn-sm bg-blue-600 hover:bg-blue-700 text-white rounded-full shadow-md md:hidden"
                 >
                   Login
@@ -248,7 +142,7 @@ const Navbar = () => {
 
       {/* Navbar End */}
       <div className="navbar-end  items-center  space-x-2 md:space-x-3">
-        {/* <ThemeToggle /> */}
+        <ThemeToggle />
 
         {loading ? (
           <div className="items-center space-x-2 hidden md:flex">
@@ -272,8 +166,7 @@ const Navbar = () => {
                 data-tooltip-place="bottom"
               />
               {/* Render Tooltip Component */}
-             <Tooltip id="avatar-tooltip" className="z-20" />
-
+              <Tooltip id="avatar-tooltip" className="z-20" />
             </div>
 
             {/* Logout Button */}
@@ -287,13 +180,14 @@ const Navbar = () => {
         ) : (
           <>
             <NavLink
-              to="/login"
+              to="/authentication/login"
               className="btn btn-sm bg-blue-600 hover:bg-blue-700 text-white rounded-full shadow-md hidden md:flex"
             >
               Login
             </NavLink>
+
             <NavLink
-              to="/register"
+              to="/authentication/register"
               className="btn btn-sm bg-blue-600 hover:bg-blue-700 text-white rounded-full shadow-md hidden md:flex"
             >
               Register
