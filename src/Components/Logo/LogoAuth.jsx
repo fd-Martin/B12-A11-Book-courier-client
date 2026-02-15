517180b264b230b116bf85c911a37a320e95de7f
fd-Martin <fdmosharof@gmail.com>
2026-02-15 09:28:43 +0600

    useAxiosSecure hook setup

---
+++ src/Components/Logo/LogoAuth.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../assets/3.png'
+
+const LogoAuth = () => {
+    return (
+        <div>
+            <img src={logo} alt="" className='h-2/3 w-2/3'/>
+        </div>
+    );
+};
+
+export default LogoAuth;
+
--- src/Layout/AuthenticationLayout.jsx
+++ src/Layout/AuthenticationLayout.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router";
+import LogoAuth from "../Components/Logo/LogoAuth";
 const AuthenticationLayout = () => {
   return (
     <div className=" w-11/12 mx-auto md:flex items-center justify-center gap-20">
       <Outlet></Outlet>
+      <LogoAuth></LogoAuth>
     </div>
   );
 };
